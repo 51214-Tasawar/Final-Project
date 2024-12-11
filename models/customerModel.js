290ae97d2576e3a188d792cb9e776cecf9e0962f
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-12-11 19:33:09 +0500

    Customer Crud

---
+++ models/customerModel.js
@@ -0,0 +1,61 @@
+
+const { where } = require("sequelize")
+const {models} = require("./index")
+
+module.exports = {
+    createCustomer : async(body)=>{
+     try{
+     const NewCustomer = await models.CustomerTable.create({...body})
+     return {
+        response : NewCustomer
+     }
+     }catch(error){
+      return{ 
+        error : error.errors[0].message 
+    }
+     }
+    } ,
+    getAll : async()=>{
+        try{
+        const NewAdmin = await models.CustomerTable.findAll()
+        return {
+           response : NewAdmin
+        }
+        }catch(error){
+         return{ 
+           error : error.errors[0].message 
+       }
+        }
+       } ,
+       UpdateCustomer : async({username, ...body})=>{
+        try{
+        const Updaterecord = await models.CustomerTable.update(
+            { ...body },{
+                where : {
+              username : username
+                }
+            })
+        return {
+           response : Updaterecord
+        }
+        }catch(error){
+         return{ 
+           error : error.errors[0].message 
+       }
+        }
+       } ,
+       DeleteCustomer : async({username})=>{
+        try{
+        const DeleteAdmin = await models.CustomerTable.destroy(
+            {where : {username : username}}
+        )
+        return {
+           response : DeleteAdmin
+        }
+        }catch(error){
+         return{ 
+           error : error.errors[0].message 
+       }
+        }
+       }
+}
