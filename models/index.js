238ff963bdc3c6f4387987850c69172d859a996d
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-12-12 21:50:35 +0500

    Retion buiting

--- models/index.js
+++ models/index.js
@@ -2,13 +2,44 @@ const AdminTable  = require("./Tables/admin") ;
 const VendorTable = require("./Tables/vendor") 
 const CustomerTable = require ("./Tables/customer")
 
-const Vendors = require("./definations/vendors")
-const products = require("./definations/products")
-const productsverations = require("./definations/productverations")
+const products = require("./Tables/products")
+const productsverations= require("./Tables/productverations")
 const variationHasAttributes = require("./definations/variationHasAttributes")
 const attributes = require("./definations/attributes")
 
 const sequelize = require("../dbconnection")
+
+
+
+// Create 1:M Relation while Vendors is Parent
+
+Vendors.hasMany(products ,  { foreignKey : "vendorId"})
+products.belongsTo(Vendors ,  { foreignKey: "vendorId"})
+
+// // product and productveriation 1: M Relationcl
+
+products.hasMany(productsverations , {foreignKey : "productId" , as :"variations"} );
+productsverations.belongsTo(products , {foreignKey: "productId",  as : "product"})
+
+// Creating M:M Relation in this method 
+
+// Vendors.belongsToMany(products , {through : "vendorsHasProducts"})
+// products.belongsToMany(Vendors , {through : "vendorsHasProducts"})
+
+// Creating Veriation and attribute relation M:M using veriationsHasattributes
+productsverations.hasMany(variationHasAttributes ,
+     {foreignKey: "productsverationsId"})
+
+variationHasAttributes.belongsTo(productsverations , 
+    {foreignKey : "productsverationsId"})
+
+attributes.hasMany(variationHasAttributes , 
+    {foreignKey :"attributeId " , as: "attributes"})
+
+variationHasAttributes.belongsTo(attributes , 
+    {foreignKey : "attributeId " , as:"attribute"})
+
+
 
 const models = {AdminTable , VendorTable , CustomerTable } 
 
