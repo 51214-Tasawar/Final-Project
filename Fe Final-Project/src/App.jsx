37317edd5875197e513ca77432fd73656a8ae938
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-12-13 20:41:39 +0500

    Code Update For Stying

--- Fe Final-Project/src/App.jsx
+++ Fe Final-Project/src/App.jsx
@@ -7,9 +7,9 @@ const App =()=>{
     <div>
       <h1>Current State Number{count}</h1>
       <button onClick={()=>setCount(count + 1)}
-        className="bg-green-500 p-2 rounded-md text-white font-light">Increement</button><br/>
+        className="bg-green-500 p-2 rounded-md text-white font-light">Increement</button>
       <button onClick={()=>setCount(count - 1)}
-        className="bg-red-500 p-2 rounded-md text-white font-light  mt-2">Substraction</button>
+        className="bg-red-500 p-2 rounded-md text-white font-light  ml-2">Substraction</button>
     </div>
     </>
   )
